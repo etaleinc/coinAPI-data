108e8c1ff8b6ab9f4bcdbe22d69499c73bfbdb35
federico buonerba <federico.buonerba@etale.com>
2018-12-07 03:01:35 +0000

    changes

--- download_coinapi_rates.py
+++ download_coinapi_rates.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import time
+import os
 import multiprocessing as mp
 import coinapi_brain as brain
 from datetime import datetime
@@ -18,11 +19,14 @@ if __name__=='__main__':
     pool=mp.Pool()
     res=[pool.apply_async(request_rates, args=(unix_time,base,quote,)) for base in coins for quote in quotes]
     out=[r.get() for r in res]
+    filename=datapath+utc[:4]+'/'+utc[4:6]+'/'+utc[6:]+'/'+utc+'.rates'
+    file_exists = os.path.isfile(filename)
     #out contains all rates. Append new rates to existing file.
     with open(datapath+utc[:4]+'/'+utc[4:6]+'/'+utc[6:]+'/'+utc+'.rates', 'a') as outfile:
         name=['time','rate','asset_id_base','asset_id_quote']
         writer=csv.DictWriter(outfile, fieldnames=name)
-        writer.writeheader()
+        if not file_exists:
+            writer.writeheader()
         for o in out:
             if o!={}:
                 writer.writerow(o)
